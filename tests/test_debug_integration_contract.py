4b796cba57b1876f934767433325a8bc4ab5ecd8
Thomas Kuhn <kuhn-thomas@mail.de>
2026-08-13 16:09:56 +0200

    Update test_debug_integration_contract.py

--- tests/test_debug_integration_contract.py
+++ tests/test_debug_integration_contract.py
@@ -29,7 +29,7 @@ class DebugIntegrationContractTests(unittest.TestCase):
             and isinstance(node.value, ast.Constant)
         )
 
-        self.assertEqual(manifest_version, "4.4.0-dev4")
+        self.assertEqual(manifest_version, "4.4.0-dev5")
         self.assertEqual(runtime_version, manifest_version)
 
     def test_services_expose_only_supported_durations(self) -> None:
